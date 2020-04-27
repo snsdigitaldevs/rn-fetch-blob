e979f51d7028da9e3937ca7d4ba96386650f87f4
Hao Zhu <hao.zhu@thoughtworks.com>
2020-04-27 13:03:51 +0800

    Update RNFetchBlobReq.java

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
@@ -597,12 +597,6 @@ public class RNFetchBlobReq extends BroadcastReceiver implements Runnable {
                 } catch (ClassCastException ex) {
                     // unexpected response type
                     if (responseBody != null) {
-                        String responseBodyString = "";
-                        try {
-                            responseBodyString = responseBody.string();
-                        } catch(IOException exception) {
-                            exception.printStackTrace();
-                        }
                         callback.invoke("Unexpected FileStorage response file.", null);
                     } else {
                         callback.invoke("Unexpected FileStorage response with no file.", null);
